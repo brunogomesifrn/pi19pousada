d937c45faba000a635dc6669878a622ca7d243bb
Andreyna <andreynapatricio12@gmail.com>
2019-11-18 09:27:05 -0300

    coisei o coisa

--- core/forms.py
+++ core/forms.py
@@ -11,14 +11,15 @@ class usuarios(UserCreationForm):
             'last_name' : forms.TextInput(attrs={'class':'form-control'}),
             'username' : forms.TextInput(attrs={'class':'form-control'}),
             'email' : forms.EmailInput(attrs={'class':'form-control'}),
-            'password1' : forms.PasswordInput(attrs={'class':'form-control'}),
-            'password2' : forms.PasswordInput(attrs={'class':'form-control'}),
+            
         }
     def __init__(self, *args, **kwargs):
         super(usuarios, self).__init__(*args, **kwargs)
-
-        for fieldname in ['first_name', 'last_name','username','email', 'password1', 'password2']:
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control'})
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control'})
+        for fieldname in ['first_name', 'last_name','username','email']:
             self.fields[fieldname].help_text = None
+
 
 #class Cursos(ModelForm):
 #class Meta():
