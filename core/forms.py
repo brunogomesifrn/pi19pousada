d3fb39c6a38ac5c4154674b31dfaedd9a0ea55c2
Andreyna <andreynapatricio12@gmail.com>
2019-11-18 09:13:39 -0300

    ajeitei banco

--- core/forms.py
+++ core/forms.py
@@ -1,11 +1,24 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
-
+from django import forms
 
 
 class usuarios(UserCreationForm):
-	class Meta(UserCreationForm.Meta):
-		fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+    class Meta(UserCreationForm.Meta):
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+        widgets = {
+            'first_name' : forms.TextInput(attrs={'class':'form-control'}),
+            'last_name' : forms.TextInput(attrs={'class':'form-control'}),
+            'username' : forms.TextInput(attrs={'class':'form-control'}),
+            'email' : forms.EmailInput(attrs={'class':'form-control'}),
+            'password1' : forms.PasswordInput(attrs={'class':'form-control'}),
+            'password2' : forms.PasswordInput(attrs={'class':'form-control'}),
+        }
+    def __init__(self, *args, **kwargs):
+        super(usuarios, self).__init__(*args, **kwargs)
+
+        for fieldname in ['first_name', 'last_name','username','email', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
 
 #class Cursos(ModelForm):
 #class Meta():
@@ -14,5 +27,5 @@ class usuarios(UserCreationForm):
 #widgets = {
 #'area' : forms.RadioSelect(),(),
 #'periodo' : forms.RadioSelect(),(),
-#'curso' : forms.RadioSelect(),(),	
+#'curso' : forms.RadioSelect(),(),  
 #}
--- core/views.py
+++ core/views.py
@@ -2,20 +2,24 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from .forms import usuarios
-
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
 	return  render(request, 'index.html') 
 
 def cadastro(request):
-	form = usuarios(request.POST or None)
-	if form.is_valid():
-		form.save()
-		return redirect('login')
-	contexto = {
-	'form': form
-	}
+	if request.method == 'POST':
+		form = usuarios(request.POST or None)
+		if form.is_valid():
+			form.save()
+			return redirect('login')
+		else:
+			return HttpResponse('erro. verifique suas informações')
+	else:
+		form = usuarios()
+	contexto = {'form': form}
+
 	return render(request, 'cadastro.html', contexto)
 
 def login(request):
